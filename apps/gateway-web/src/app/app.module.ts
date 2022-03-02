96ec20748a7381d2d6b73ed5f7351143fcaa311a
iamlookod <preecharat.lokod@gmail.com>
2022-03-02 22:26:24 +0700

    Add prisma module.

--- apps/gateway-web/src/app/app.module.ts
+++ apps/gateway-web/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { EnvModule } from '@nest-microservice-boilerplate/env';
 import { HealthModule } from '@nest-microservice-boilerplate/health';
+import { PrismaModule } from '@nest-microservice-boilerplate/prisma';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [HealthModule, EnvModule],
+  imports: [HealthModule, EnvModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
